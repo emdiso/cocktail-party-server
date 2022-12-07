651cbf39532355139d43acc81d906bf317fb74f3
cjc436 <97143146+cjc436@users.noreply.github.com>
2022-12-06 20:28:39 -0500

    pushing correct fix for psql connection ssl

--- dist/utils/psqlConnection.js
+++ dist/utils/psqlConnection.js
@@ -4,12 +4,12 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    // ssl: {
-    //     rejectUnauthorized: false,
-    // },
-});
+const pool = new Pool(isProduction ? {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+} : { connectionString: connectionString });
 // const psqlEnv = require("../../env.json"); // Convert to typsecript import statment
 // let psqlPool = new Pool(psqlEnv);
 // psqlPool.connect().then(() => {
--- utils/psqlConnection.ts
+++ utils/psqlConnection.ts
@@ -4,12 +4,12 @@ require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
 
 const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+const pool = new Pool(isProduction ? {
+    connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: !isProduction,
+        rejectUnauthorized: false,
     },
-});
+} : {connectionString: connectionString});
 
 // const psqlEnv = require("../../env.json"); // Convert to typsecript import statment
 // let psqlPool = new Pool(psqlEnv);
