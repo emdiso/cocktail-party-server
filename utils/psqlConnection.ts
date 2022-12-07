11aed9b09ab8c1fe43b7e882f3d17c50724ea6fb
emdiso <emdisomma@gmail.com>
2022-12-06 21:11:22 -0500

    didnt work still here

--- utils/psqlConnection.ts
+++ utils/psqlConnection.ts
@@ -4,12 +4,12 @@ require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
 
 const connectionString = process.env.DATABASE_URL;
-const psqlPool = new Pool(isProduction ? {
+const psqlPool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     },
-} : {connectionString: connectionString});
+});
 
 psqlPool.connect().then(() => {
   console.log("Connected to database");
