793bf668729bdb2653d1b4f9bc7da088692d2280
emdiso <emdisomma@gmail.com>
2022-12-06 21:07:57 -0500

    if this works i quit

--- utils/psqlConnection.ts
+++ utils/psqlConnection.ts
@@ -4,15 +4,15 @@ require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
 
 const connectionString = process.env.DATABASE_URL;
-const pool = new Pool(isProduction ? {
+const psqlPool = new Pool(isProduction ? {
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     },
 } : {connectionString: connectionString});
 
-pool.connect().then(() => {
+psqlPool.connect().then(() => {
   console.log("Connected to database");
 });
 
-export default pool;
+export default psqlPool;
