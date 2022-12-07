5063d19bf44d3a0e893bf198275522ed4171417a
emdiso <emdisomma@gmail.com>
2022-12-06 21:31:44 -0500

    added back

--- utils/psqlConnection.ts
+++ utils/psqlConnection.ts
@@ -2,14 +2,13 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const isProduction = process.env.NODE_ENV === "production";
-
 const connectionString = process.env.DATABASE_URL;
-const psqlPool = new Pool({
+const psqlPool = new Pool(isProduction ? {
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     },
-});
+} : {connectionString: connectionString});
 
 psqlPool.connect().then(() => {
   console.log("Connected to database");
